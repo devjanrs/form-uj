ebd78db6e50f515f52b1f9b6a1470c6c81759f8a
janspecter <jrs2021.98@gmail.com>
2024-05-23 17:09:50 -0300

    added  uform and yup

--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,73 @@
 <script setup lang="ts">
-    const method = ['Pix', 'Dinheiro', 'Débito', 'Crédito']
+  const method = ['Pix', 'Dinheiro', 'Débito', 'Crédito']
 
-    const selected = ref(method[4])
+  const selected = ref(method[4])
 
-    const religion = ['Evangélico', 'Católico', 'Espírita', 'Outros', 'Não possuo']
+  const religion = ['Evangélico', 'Católico', 'Espírita', 'Outros', 'Não possuo']
 
-    const selected_religion = ref(religion[5])
+  const selected_religion = ref(religion[5])
 
-    const options1 = [{
+  const options1 = [{
+  value: 'Sim',
+  label: 'Sim'
+  }, {
+    value: 'Não',
+    label: 'Não'
+  }]
+
+  const selected1 = ref('')
+
+  const options2 = [{
     value: 'Sim',
     label: 'Sim'
-    }, {
-      value: 'Não',
-      label: 'Não'
-    }]
+  }, {
+    value: 'Não',
+    label: 'Não'
+  }]
 
-    const selected1 = ref('')
+  const selected2 = ref('')
 
-    const options2 = [{
-      value: 'Sim',
-      label: 'Sim'
-    }, {
-      value: 'Não',
-      label: 'Não'
-    }]
+  const options3 = [{
+    value: 'Sim',
+    label: 'Sim'
+  }, {
+    value: 'Não',
+    label: 'Não'
+  }]
 
-    const selected2 = ref('')
+  const selected3 = ref('')
 
-    const options3 = [{
-      value: 'Sim',
-      label: 'Sim'
-    }, {
-      value: 'Não',
-      label: 'Não'
-    }]
+  const colorMode = useColorMode()
+  const isDark = computed({
+    get () {
+      return colorMode.value === 'dark'
+    },
+    set () {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    }
+  })
 
-    const selected3 = ref('')
+  import { object, string, type InferType } from 'yup'
+  import type { FormSubmitEvent } from '#ui/types'
 
-    const colorMode = useColorMode()
-    const isDark = computed({
-      get () {
-        return colorMode.value === 'dark'
-      },
-      set () {
-        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-      }
-    })
+  const schema = object({
+    email: string().email('Invalid email').required('Required'),
+    password: string()
+      .min(8, 'Must be at least 8 characters')
+      .required('Required')
+  })
 
+  type Schema = InferType<typeof schema>
+
+  const state = reactive({
+    email: undefined,
+    password: undefined
+  })
+
+  async function onSubmit (event: FormSubmitEvent<Schema>) {
+    // Do something with event.data
+    console.log(event.data)
+  }
 </script>
 
 <template>
@@ -78,74 +99,78 @@
     </template>
   </ClientOnly>
 
-  <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
-    <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Informações Pessoais</h1>
-    <div class="rounded">
-      <UFormGroup size="xl" label="Nome completo" name="name" required>
-        <UInput/>
-      </UFormGroup>
-  
-      <UFormGroup size="xl" label="RG" name="rg" required :ui="{wrapper: 'mt-4'}">
-        <UInput/>
-      </UFormGroup>
-  
-      <UFormGroup size="xl" label="CPF" name="cpf" required :ui="{wrapper: 'mt-4'}">
-        <UInput/>
-      </UFormGroup>
-  
-      <UFormGroup size="xl" label="Data de Nascimento" name="birth" required :ui="{wrapper: 'mt-4'}">
-        <UInput type="date"/>
-      </UFormGroup>
-  
-      <UFormGroup size="xl" label="Tel. Residencial" required name="tel1" :ui="{wrapper: 'mt-4'}">
-        <UInput/>
-      </UFormGroup>
-  
-      <UFormGroup size="xl" label="Tel. Celular" required name="tel2" :ui="{wrapper: 'mt-4'}">
-        <UInput/>
-      </UFormGroup>
+  <UForm :schema="schema" :state="state" class="border-neutral-200 md:border flex flex-col gap-4 md:p-6 rounded" @submit="onSubmit">
+    <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
+      <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Informações Pessoais</h1>
+      <div class="rounded">
+        <UFormGroup size="xl" label="Nome completo" name="name" required>
+          <UInput/>
+        </UFormGroup>
+    
+        <UFormGroup size="xl" label="RG" name="rg" required :ui="{wrapper: 'mt-4'}">
+          <UInput/>
+        </UFormGroup>
+    
+        <UFormGroup size="xl" label="CPF" name="cpf" required :ui="{wrapper: 'mt-4'}">
+          <UInput/>
+        </UFormGroup>
+    
+        <UFormGroup size="xl" label="Data de Nascimento" name="birth" required :ui="{wrapper: 'mt-4'}">
+          <UInput type="date"/>
+        </UFormGroup>
+    
+        <UFormGroup size="xl" label="Tel. Residencial" required name="tel1" :ui="{wrapper: 'mt-4'}">
+          <UInput/>
+        </UFormGroup>
+    
+        <UFormGroup size="xl" label="Tel. Celular" required name="tel2" :ui="{wrapper: 'mt-4'}">
+          <UInput/>
+        </UFormGroup>
+      </div>
     </div>
-  </div>
 
-  <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
-    <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Forma de pagamento (Não aceitamos cheque)</h1>
-    <p class="text-sm font-medium">Valor R$430,00</p>
-    <div class="rounded">
-      <USelectMenu v-model="selected" :options="method" placeholder="Selecione o método de pagamento desejado" name="pay"/>
+    <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
+      <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Forma de pagamento (Não aceitamos cheque)</h1>
+      <p class="text-sm font-medium">Valor R$430,00</p>
+      <div class="rounded">
+        <USelectMenu v-model="selected" :options="method" placeholder="Selecione o método de pagamento desejado" name="pay"/>
+      </div>
     </div>
-  </div>
 
-  <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
-    <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Dados Médicos</h1>
-    <div class="rounded">
-      <URadioGroup :ui="{wrapper: 'mt-1'}" v-model="selected1" legend="Necessita de cuidados médicos?" :options="options1" name="data1"/>
-      <URadioGroup :ui="{wrapper: 'mt-4'}" v-model="selected2" legend="Necessita de medicamentos?" :options="options2" name="data2"/>
-      <URadioGroup :ui="{wrapper: 'mt-4'}" v-model="selected3" legend="É alergico?" :options="options3" name="data3"/>
+    <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
+      <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Dados Médicos</h1>
+      <div class="rounded">
+        <URadioGroup :ui="{wrapper: 'mt-1'}" v-model="selected1" legend="Necessita de cuidados médicos?" :options="options1" name="data1"/>
+        <URadioGroup :ui="{wrapper: 'mt-4'}" v-model="selected2" legend="Necessita de medicamentos?" :options="options2" name="data2"/>
+        <URadioGroup :ui="{wrapper: 'mt-4'}" v-model="selected3" legend="É alergico?" :options="options3" name="data3"/>
+      </div>
     </div>
-  </div>
 
-  <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
-    <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Religião</h1>
-    <div class="rounded">
-      <USelectMenu v-model="selected_religion" :options="religion" placeholder="Selecione sua religião"ame="religionchoice"/>
+    <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1">
+      <h1 class="bg-teal-800 text-white p-2 rounded font-medium">Religião</h1>
+      <div class="rounded">
+        <USelectMenu v-model="selected_religion" :options="religion" placeholder="Selecione sua religião"ame="religionchoice"/>
+      </div>
     </div>
-  </div>
 
-  <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1 rounded">
-    <h1 class="text-red-700 font-semibold p-1 rounded mb-1 text-base">
-      NÃO NOS RESPONSABILIZAMOS PELA PERDA OU EXTRAVIO DE OBJETOS PESSOAIS.
-      A GUARDA E RESPONSABILIDADE DOS MESMOS É EXCLUSIVAMENTE DO ENCONTRISTA.</h1>
-  </div>
+    <div class="flex flex-col gap-2 p-2 w-1/2 mx-auto mt-1 rounded">
+      <h1 class="text-red-700 font-semibold p-1 rounded mb-1 text-base">
+        NÃO NOS RESPONSABILIZAMOS PELA PERDA OU EXTRAVIO DE OBJETOS PESSOAIS.
+        A GUARDA E RESPONSABILIDADE DOS MESMOS É EXCLUSIVAMENTE DO ENCONTRISTA.</h1>
+    </div>
 
-  <UButton class="mx-auto flex mt-4 mb-5 bg-teal-800"
-    icon="i-heroicons-pencil-square"
-    size="sm"
-    color="primary"
-    variant="solid"
-    label="Realizar inscrição"
-    trailing
-  />
-
+    <UButton
+      type="submit"
+      class="mx-auto flex mt-4 mb-5 bg-teal-800"
+      icon="i-heroicons-pencil-square"
+      size="sm"
+      color="primary"
+      variant="solid"
+      label="Realizar inscrição"
+      trailing
+    />
+  </UForm>
+  
   <footer class="bg-teal-800 flex flex-col p-2">
     <h1 class="mx-auto flex mt-2 text-sm font-medium text-white"
       >Fique por dentro de todas as novidades seguindo o nosso instagram</h1>
